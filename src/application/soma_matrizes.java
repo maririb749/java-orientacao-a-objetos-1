ad747e95bdd15ce9f66687b1c78a16149f89b67f
Mariana Santos <maririb51@gmail.com>
2022-07-21 14:11:07 +0100

    Exercicio Matrizes(Soma_matrizes)

---
+++ src/application/soma_matrizes.java
@@ -0,0 +1,73 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class soma_matrizes {
+
+	/*
+	 * Fazer um programa para ler duas matrizes de números inteiros A e B, contendo
+	 * de M linhas e N colunas cada (M e N máximo = 10). Depois, gerar uma terceira
+	 * matriz C onde cada elemento desta é a soma dos elementos correspondentes das
+	 * matrizes originais. Imprimir na tela a matriz gerada.
+	 */
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int m,n;
+		
+		
+		System.out.print("Quantas linhas vai ter cada matriz? ");
+		m = sc.nextInt();
+		
+		System.out.print("Quantas colunas vai ter cada matriz?");
+		n = sc.nextInt();
+		
+		int [][] a = new int [m][n];
+		int [][] b = new int [m][n];
+        int	[][] c = new int [m][n];
+        
+        System.out.println("Digite os valores da matriz A:");
+        
+        for(int i=0; i<m; i++) {
+        	for(int j=0; j<n; j++) {
+        		System.out.printf("Elemento [%d,%d]:",i,j);
+        		a[i][j] = sc.nextInt();
+        		
+        	}
+        	
+        }
+         System.out.println("Digite os valores da matriz B:");  
+         
+         for(int i=0; i<m; i++) {
+        	 for(int j=0; j<n; j++) {
+        		 System.out.printf("Elemento [%d,%d]:",i,j);
+        		 b[i][j] = sc.nextInt();
+        	 }
+         }
+		
+         for(int i=0; i<m; i++) {
+        	 for(int j=0; j<n; j++) {
+        		 c[i][j] = a[i][j] + b[i][j];
+        	 }
+         }
+         
+         System.out.println("MATRIZ SOMA:");
+         
+         for(int i=0; i<m; i++) {
+        	 for(int j=0; j<n; j++) {
+        		 System.out.printf("%d ", c[i][j]);
+        	 }
+        	 System.out.println();
+         }
+         
+        
+		sc.close();
+	}
+	
+}
+
+
