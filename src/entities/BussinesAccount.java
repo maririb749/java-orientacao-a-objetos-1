533f2a95b4788e4a0f9bce81f65e4e781c2f05a2
Mariana Santos <maririb51@gmail.com>
2022-07-26 10:36:21 +0100

    Herança

---
+++ src/entities/BussinesAccount.java
@@ -0,0 +1,39 @@
+package entities;
+
+public class BussinesAccount extends Account {
+
+	private Double loanLimit;
+
+	public BussinesAccount() {
+		super();
+
+	}
+
+	public BussinesAccount(Integer number, String holder, Double balance, Double loanLimit) {
+		super(number, holder, balance);
+		this.loanLimit = loanLimit;
+	}
+
+	public Double getLoanLimit() {
+		return loanLimit;
+	}
+
+	public void setLoanLimit(Double loanLimit) {
+		this.loanLimit = loanLimit;
+	}
+
+	/*
+	 * suponha que para realizar um empréstimo,
+	 * é descontada uma taxa no valor de 10.0;
+	 */
+
+	public void loan(double amount) {
+		if (amount <= loanLimit) {
+			balance += amount - 10.0;
+			
+
+		}
+
+	}
+
+}
